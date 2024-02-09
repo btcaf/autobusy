75abd724bcf12a278b91f800aa1dadb760a9e0d0
btcaf <c.botta2003@gmail.com>
2024-02-09 05:34:09 +0100

    removed unnecessary things

--- autobusy/downloader/util.py
+++ autobusy/downloader/util.py
@@ -1,13 +1,3 @@
-import requests
-import json
-import time
-
-URL = 'https://api.um.warszawa.pl/api/action/busestrams_get'
-RESOURCE_ID = 'f2e5503e-927d-4ad3-9500-4ab9e55deb59'
-TIMEOUT = 10
-TYPE = 1
-
-
 def check_list_of_hours(hours: list[int]):
     if len(hours) > 24:
         raise ValueError('List of hours is too long')
