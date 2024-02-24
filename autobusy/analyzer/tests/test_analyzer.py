47b51c593250648a382fcf814dc06f35ab05804d
btcaf <c.botta2003@gmail.com>
2024-02-24 02:51:47 +0100

    more tests

--- autobusy/analyzer/tests/test_analyzer.py
+++ autobusy/analyzer/tests/test_analyzer.py
@@ -2,3 +2,24 @@ from autobusy.analyzer.analyzer import Analyzer, Results
 import pytest
 
 
+@pytest.mark.parametrize("routes, expectation", [
+    ({
+         '1': [['1', '2', '3'], ['3', '2', '1']],
+     }, {
+         '1': [['1', '2', '3'], ['3', '2', '1']],
+     }),
+    ({
+         '1': [['1', '2', '3', '4'], ['1', '3', '4'], ['3', '2', '1'], ['2', '1']],
+     }, {
+         '1': [['1', '2', '3', '4'], ['3', '2', '1']],
+     }),
+    ({
+         '1': [['1', '2', '3', '4'], ['1', '3', '4'], ['3', '2', '1'], ['2', '1']],
+         '2': [['1', '2', '3', '4'], ['1', '3', '4']],
+     }, {
+         '1': [['1', '2', '3', '4'], ['3', '2', '1']],
+         '2': [['1', '2', '3', '4']],
+     }),
+])
+def test_get_max_opposite_routes(routes, expectation):
+    assert Analyzer.get_max_opposite_routes(routes) == expectation
